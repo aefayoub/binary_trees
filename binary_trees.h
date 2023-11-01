69a008151651976ff4c46b603dbec18d1dab6bc6
aefayoub <aef.ayoub@gmail.com>
2023-11-01 07:46:12 -0700

    cmt header

--- binary_trees.h
+++ binary_trees.h
@@ -40,4 +40,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 /*inserts a node as the left-child of another node*/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
+/*inserts a node as the right-child*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
 #endif /* _BINARY_TREES_H_ */
