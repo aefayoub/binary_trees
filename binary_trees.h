afe3363290d74ccfbf581d9ee3883c863bb84890
aefayoub <aef.ayoub@gmail.com>
2023-11-01 06:54:55 -0700

    cmt betty changes of  header

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef _BINARY_TREES
-#define _BINARY_TRRES
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 #include <stddef.h>
 #include <stdlib.h>
@@ -14,10 +14,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -37,4 +37,4 @@ void binary_tree_print(const binary_tree_t *tree);
 /*binary_tree_node*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
-#endif
+#endif /* _BINARY_TREES_H_ */
