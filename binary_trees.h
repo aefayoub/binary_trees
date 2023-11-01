e4234e83d6b08d5af94d9386f2820299dadfd479
aefayoub <aef.ayoub@gmail.com>
2023-11-01 07:10:16 -0700

    cmt task 1 and declarration of function

--- binary_trees.h
+++ binary_trees.h
@@ -37,4 +37,7 @@ void binary_tree_print(const binary_tree_t *tree);
 /*binary_tree_node*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
+/*inserts a node as the left-child of another node*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
 #endif /* _BINARY_TREES_H_ */
