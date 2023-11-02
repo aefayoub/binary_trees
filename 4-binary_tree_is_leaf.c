0327804b204c4994aa0b92f1d45eced774c70a5a
aefayoub <aef.ayoub@gmail.com>
2023-11-02 05:39:25 -0700

    cmt task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	int leaf = 0;
+
+	if (node && !(node->left) && !(node->right))
+		leaf = 0;
+
+	return(leaf);
+}
