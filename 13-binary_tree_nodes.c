1789077703a2dbb59fc4820a6d3e896115f33c6e
aefayoub <aef.ayoub@gmail.com>
2023-11-02 13:53:31 -0700

    cmt task 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * tree_is_leaf - Measure the height of a binary tree
+ * @tree: A pointer to the root node of the tree to measure
+ * Return: The height of the tree, or 0 if the tree is NULL
+ */
+int tree_is_parent(const binary_tree_t *node)
+{
+	int parent = 0;
+
+	if (node && (node->left || node->right))
+		parent = 1;
+
+	return (parent);
+}
+
+/**
+ * binary_tree_nodes - Measure the height of a binary tree
+ * @tree: A pointer to the root node of the tree to measure
+ * Return: The height of the tree below the root, or 0 if the tree is NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree_is_parent(tree))
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+
+	return (0);
+}
