ab422e012f9e9c229f1612ddbf379e30660c052f
aefayoub <aef.ayoub@gmail.com>
2023-11-02 08:13:38 -0700

    cmt header

--- binary_trees.h
+++ binary_trees.h
@@ -61,4 +61,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 /*a binary tree using post-order traversal*/
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
+/*measures the height of a binary tree*/
+size_t binary_tree_height(const binary_tree_t *tree);
+
 #endif /* _BINARY_TREES_H_ */
