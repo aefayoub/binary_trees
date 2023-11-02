5d2fd91a6ebaaafdb1600390a69d121bdfc93f61
aefayoub <aef.ayoub@gmail.com>
2023-11-02 06:29:49 -0700

    cmt header

--- binary_trees.h
+++ binary_trees.h
@@ -52,4 +52,7 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 /*checks if a node is a root*/
 int binary_tree_is_root(const binary_tree_t *node);
 
+/*binary tree using pre-order traversal*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
 #endif /* _BINARY_TREES_H_ */
