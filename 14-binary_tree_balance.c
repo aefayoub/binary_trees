daa61b6d2bf5dddb0199a6b12c5a6e45c18f365e
aefayoub <aef.ayoub@gmail.com>
2023-11-03 06:45:20 -0700

    cmt task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,46 @@
+#include "binary_trees.h"
+
+/**
+ * count_height - Measure the height of a binary tree
+ * @tree: A pointer to the root node of the tree to measure
+ * Return: The height of the tree, or 0 if the tree is NULL
+ */
+size_t count_height(const binary_tree_t *tree)
+{
+	size_t left_h, right_h;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	right_h = count_height(tree->right);
+	left_h = count_height(tree->left);
+
+	if (left_h > right_h)
+		return (left_h + 1);
+
+	return (right_h + 1);
+
+}
+
+/**
+ * binary_tree_height - Measure the height of a binary tree
+ * @tree: A pointer to the root node of the tree to measure
+ * Return: The height of the tree below the root, or 0 if the tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (-1);
+
+	return (count_height(tree) - 1);
+}
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
