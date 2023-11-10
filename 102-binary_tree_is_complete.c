507bedc0cb60a46178dba941e690ee4090405875
aefayoub <aef.ayoub@gmail.com>
2023-11-10 02:21:29 -0800

    cmt task 102

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -14,6 +14,14 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
 
+/**
+ * is_complete - checks if tree is complete
+ *
+ * @tree: pointer to the tree root
+ * @i: node index
+ * @cnodes: number of nodes
+ * Return: 1 if tree is complete, 0 otherwise
+ */
 int is_complete(const binary_tree_t *tree, int i, int nodes)
 {
 	if (tree == NULL)
@@ -25,6 +33,12 @@ int is_complete(const binary_tree_t *tree, int i, int nodes)
 	return (is_complete(tree->left, (2 * i) + 1, nodes) && is_complete(tree->right, (2 * i) + 2, nodes));
 }
 
+/**
+ * binary_tree_is_complete - calls to tree_is_complete function
+ *
+ * @tree: tree root
+ * Return: 1 if tree is complete, 0 otherwise
+ */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	size_t nodes;
